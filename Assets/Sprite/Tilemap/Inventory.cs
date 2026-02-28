1a3747c957aa1c9f9de8fe90038caf034e6c702f
Blake Williams <blake14williams@gmail.com>
2026-02-28 12:11:26 -0500

    beginInv

--- Assets/Sprite/Tilemap/Inventory.cs
+++ Assets/Sprite/Tilemap/Inventory.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 
   public class InvItem
     {
-        public InvItem()
-    {
+
+        public string name;
+        public Sprite sprite;
+        public InvItem(string ItemName, Sprite ItemSprite)
+        {
+            name = ItemName;
+            sprite = ItemSprite;
+        }
+    
         
-    }
+    
     }
 public class Inventory : MonoBehaviour
 {
 
-
+    public InvItem[] inventory;
   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,4 +31,5 @@ public class Inventory : MonoBehaviour
     {
         
     }
+
 }
